cafaf72759924baccbd8ee1d0231ea56ecb917d8
Islomjon-2013 <hulkarsiddiqova747@gmail.com>
2022-10-10 18:42:27 -0400

    added some codes to js file

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ document.querySelector(".btn").addEventListener("click", function () {
   console.log(typeof parseInt(ekran.value));
   ekran.value = parseInt(ekran.value) + 1;
 });
+document.querySelector(".btn-2").addEventListener("click", function () {
+  document.querySelector(".ekran").value = 0;
+});
